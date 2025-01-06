c8821be60083b9185b6bc465279c9a454ccafeb9
Keith T. Garner <kgarner@kgarner.com>
2025-01-06 11:00:26 -0600

    remove slack detection that just did not work

--- lib/legacy_zoom_detection.rb
+++ lib/legacy_zoom_detection.rb
@@ -5,11 +5,6 @@ require "sys/proctable"
 
 class LegacyZoomDetection
   ZOOM_PROCESS = /CptHost$/.freeze
-  # Slack doesn't seem to start a help in process for just audio... well, they do, but its started shortly after slack
-  # starts.. so this will detect if video capture is running, so we'll assume that's a meeting, especially since I'm a
-  # camera on kind of guy.
-  SLACK_PROCESS = /Slack Helper \(Plugin\)$/.freeze
-  SLACK_ARG = /utility-sub-type=video_capture.mojom.VideoCaptureService/.freeze
 
   def initialize(options)
     @options = options
@@ -18,7 +13,6 @@ class LegacyZoomDetection
   def meeting_active?
     processes = Sys::ProcTable.ps(thread_info: false)
 
-    # processes.any? { |p| p.exe =~ ZOOM_PROCESS || (p.exe =~ SLACK_PROCESS && p.cmdline =~ SLACK_ARG) }
     processes.any? { |p| p.exe =~ ZOOM_PROCESS }
   end
 
