7220ef5f7774c594296350910205d0db18a6c036
Keith T. Garner <kgarner@kgarner.com>
2025-11-11 17:14:28 -0500

    rewrite MacOSLogStreamDetection
    This should now catch a wider variety of camera activation and now picks up solo mic activation. However full mic deactiviation seems to be a lagging log entry by a few seconds.

--- lib/macos_log_stream_detection.rb
+++ lib/macos_log_stream_detection.rb
@@ -1,21 +1,44 @@
 # frozen_string_literal: true
 
-require 'forwardable'
+require "English"
+require "forwardable"
 require "zoom_activity_publisher"
-require 'concurrent/scheduled_task'
+require "concurrent/scheduled_task"
+require "json"
+
+# logging data on camera and mic from https://github.com/objective-see/OverSight/blob/main/Application/Application/AVMonitor.m#L214
 
 class MacOSLogStreamDetection
-  # The stream command is only catching the camera, not mic. I still haven't figure out a sane/simple way to detect mic only usage.
-  # TODO: figure out mic only usage
-  # rubocop:disable Layout/LineLength
-  MACOS_COMMAND = %{/usr/bin/log stream --predicate '(eventMessage CONTAINS "<<<< AVCaptureSession >>>> -[AVCaptureSession_Tundra startRunning]" || eventMessage CONTAINS "<<<< AVCaptureSession >>>> -[AVCaptureSession_Tundra stopRunning]")'}
-  SESSION_RE = /(?<pid>\d+)\s+\d+\s+(?<command>[\w\.\-\(\))]+(?<_>\s[\w\.\-\(\))]+)*): \(AVFCapture\) \[[\w\.]+:\] <<<< AVCaptureSession >>>> -\[AVCaptureSession_Tundra (?<status>start|stop)Running\]/
-  # rubocop:enable Layout/LineLength
+  # Yes, this is gross AF, but I couldn't think of a better way to get the command to be understandable and easily
+  # editble
+  def self.macos_command
+    @macos_command ||= begin
+      command = <<~FOO
+        /usr/bin/log stream
+          --predicate '(subsystem=="com.apple.cmio"
+                        AND
+                        (eventMessage CONTAINS "added <private> endpoint <private> camera <private>"
+                        OR
+                        eventMessage CONTAINS "removed endpoint <private>"))
+                      OR
+                      (subsystem=="com.apple.coremedia"
+                        AND
+                        (eventMessage CONTAINS "-[MXCoreSession beginInterruption]:"
+                        OR
+                        eventMessage CONTAINS "-[MXCoreSession endInterruption:]:"))'
+          --style ndjson
+      FOO
+      command.tr("\n", " ").squeeze(" ")
+    end
+  end
 
-  attr_reader :publisher, :logger, :capturers
+  CAMARA_ON_RE = /added <private> endpoint <private> camera <private>/
+  CAMARA_OFF_RE = /removed endpoint <private>/
+  MIC_ON_RE = /-\[MXCoreSession beginInterruption\]: Session <ID: (?<id>\d+), PID = (?<pid>\d+),/
+  MIC_OFF_RE = /-\[MXCoreSession endInterruption:\]: Session <ID: (?<id>\d+), PID = (?<pid>\d+),/
 
-  # sometimes the same app/pid can open the camera stream multiple times, so use this counter to track them.
-  # TODO: if this approach works better, move this into its own file
+  attr_reader :publisher, :logger, :mic_capturers
+
   class PidCounter
     extend Forwardable
 
@@ -25,56 +48,70 @@ class MacOSLogStreamDetection
       @pids = Hash.new { |h, k| h[k] = 0 }
     end
 
-    def add(pid)
-      @pids[pid] += 1
+    def add(session, pid)
+      @pids[[session, pid]] += 1
     end
 
-    def remove(pid)
-      @pids[pid] -= 1
-      @pids.delete(pid) if @pids[pid] <= 0
+    def remove(session, pid)
+      key = [session, pid]
+      @pids[key] -= 1
+      @pids.delete(key) if @pids[key] <= 0
     end
   end
 
   def initialize(publisher:, logger:)
     @publisher = publisher
     @logger = logger
-    @capturers = PidCounter.new
+    @mic_capturers = PidCounter.new
   end
 
   def run
     task = nil
-    IO.popen(MACOS_COMMAND) do |io|
+    camera_count = 0
+
+    logger.debug { "Starting log stream with command: #{MacOSLogStreamDetection.macos_command}" }
+    IO.popen(MacOSLogStreamDetection.macos_command) do |io|
       io.readline # skip the header line from the command
       io.each_line do |line|
-        md = line.match(SESSION_RE)
-        next unless md
+        json = JSON.parse(line)
 
-        pid = md[:pid].to_i
+        event_message = json["eventMessage"]
 
-        case md[:status]
-        when "start"
-          logger.debug { "start for #{pid} #{md[:command]}" }
-          capturers.add(pid)
-        when "stop"
-          logger.debug { "stop for #{pid} #{md[:command]}" }
-          capturers.remove(pid)
-        else
-          logger.error { "No action! Couldn't interpret: #{line}" }
+        case json["subsystem"]
+        when "com.apple.cmio"
+          case event_message
+          when CAMARA_ON_RE
+            logger.debug { "camera ON detected" }
+            camera_count += 1
+          when CAMARA_OFF_RE
+            logger.debug { "camera OFF detected" }
+            camera_count -= 1 if camera_count > 0
+          end
+        when "com.apple.coremedia"
+          case event_message
+            # matchdata is in $~
+          when MIC_ON_RE
+            logger.debug { "mic ON detected" }
+            mic_capturers.add($LAST_MATCH_INFO[:id], $LAST_MATCH_INFO[:pid])
+          when MIC_OFF_RE
+            logger.debug { "mic OFF detected" }
+            mic_capturers.remove($LAST_MATCH_INFO[:id], $LAST_MATCH_INFO[:pid])
+          end
         end
 
-        logger.debug { "capturers: #{capturers} should_be_on: #{capturers.any?}" }
+        logger.debug { "camera count: #{camera_count}, capturers: #{mic_capturers}" }
 
         # Probably an overly complicated way to debounce
-        unless task&.pending?
+        if task&.pending?
+          logger.debug { "Task already scheduled, not scheduling again" }
+        else
           logger.debug { "Scheduling task to set status" }
           task = Concurrent::ScheduledTask.new(1) do
-            should_be_on = capturers.any?
+            should_be_on = mic_capturers.any? || camera_count.positive?
             publisher.status = should_be_on if publisher.status != should_be_on
             logger.debug { "Setting status to #{should_be_on}" }
           end
           task.execute
-        else
-          logger.debug { "Task already scheduled, not scheduling again" }
         end
       end
     end
